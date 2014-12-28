ad261308d4f34ed2fe3f3b079b5091c3b5de3309
Danton <dn95 _at_ georgetown.edu>
2014-12-28 09:09:27 -0300

    the master piece
    perfection. runs only python code blocks!

--- expands.py
+++ expands.py
@@ -21,18 +21,26 @@ class ExpandSelectionToSectionCommand(sublime_plugin.TextCommand):
             return v.substr(v.line(v.text_point(row, col)))
 
         def find_top(line, pattern):
-            top = None
-            while top is None:
-                line = line - 1
-                if pattern.match(get_text(line, 0)): # match text
-                    return v.line(v.text_point(line + 1, 0))
+            print('%d' % line)
+            if pattern.match(get_text(line, 0)): # match text
+                top = v.line(v.text_point(line + 1, 0))
+                print('section values: (%d, %d)' % (top.a, top.b))
+                return top
+            else:
+                return find_top(line - 1, pattern)
 
-        def find_bottom(line, pattern):
-            top = None
-            while top is None:
-                line = line + 1
-                if pattern.match(get_text(line, 0)): # match text
-                    return v.line(v.text_point(line - 1, 0))
+        def find_bottom(line, eof_line, pattern):
+            print('%d' % line)
+            if pattern.match(get_text(line, 0)): # match text
+                bottom = v.line(v.text_point(line - 1, 0))
+                print('section values: (%d, %d)' % (bottom.a, bottom.b))
+                return bottom
+            elif line == eof_line:
+                bottom = v.line(v.text_point(eof_line, 0))
+                print('eof values: (%d, %d)' % (bottom.a, bottom.b))
+                return bottom
+            else:
+                return find_bottom(line + 1, eof_line, pattern)
 
         # find pattern
         import re
@@ -41,8 +49,10 @@ class ExpandSelectionToSectionCommand(sublime_plugin.TextCommand):
         # search for closest section top and bottom
         first_point = v.line(s[0]).a
         current_line = v.rowcol(first_point)[0] # get line number
+        eof = v.size()
+        eof_line = v.rowcol(eof)[0]
         top_line = find_top(current_line, pattern)
-        bottom_line = find_bottom(current_line, pattern)
+        bottom_line = find_bottom(current_line, eof_line, pattern)
         section = sublime.Region(top_line.a, bottom_line.b)
 
         s.clear()
--- pydev.py
+++ pydev.py
@@ -4,10 +4,10 @@ import sublime, sublime_plugin
 class PyColCommand(sublime_plugin.WindowCommand):
     def run(self):
         self.window.run_command('set_layout', {
-        	"cols": [0.0, 0.5, 1.0],
-			"rows": [0.0, 1.0],
-			"cells": [[0, 0, 1, 1], [1, 0, 2, 1]]
-		})
+            "cols": [0.0, 0.5, 1.0],
+            "rows": [0.0, 1.0],
+            "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]
+        })
         self.window.run_command('run_existing_window_command', {
             "id": "repl_python_ipython",
             "file": "config/Python/Main.sublime-menu"
@@ -18,43 +18,148 @@ class PyColCommand(sublime_plugin.WindowCommand):
 class PyRowCommand(sublime_plugin.WindowCommand):
     def run(self):
         self.window.run_command('set_layout', {
-        	"cols":[0.0, 1.0],
-        	"rows":[0.0, 0.5, 1.0],
-        	"cells":[[0, 0, 1, 1], [0, 1, 1, 2]]
-    	})
+            "cols":[0.0, 1.0],
+            "rows":[0.0, 0.5, 1.0],
+            "cells":[[0, 0, 1, 1], [0, 1, 1, 2]]
+        })
         self.window.run_command('run_existing_window_command', {
             "id": "repl_python_ipython",
             "file": "config/Python/Main.sublime-menu"
         })
-        self.window.run_command('move_to_group', { "group": 1 })
+        self.window.run_command('move_to_group', {"group": 1})
 
-class ReplViewAndExecuteCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        self.window.run_command('repl_transfer_current', {
+class ReplViewAndExecute(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        # get largest selected point and eof
+        v = self.view
+        max_point = v.sel()[0].end() # largest selected point
+        max_point_line_num = v.rowcol(max_point)[0] # line num of max point
+        max_point_region = v.line(v.text_point(max_point_line_num, 0)) # region of max point
+        eof_line = v.rowcol(v.size())[0] # get the eof line
+
+        # order and remove blocked comments and empty space (keep only code blocks)
+        self.keep_code_blocks()
+
+        v.window().run_command('repl_transfer_current', {
             "scope": "lines",
             "action": "view_write"
         }) # send lines to REPL
-        self.window.run_command('focus_group', {"group": 1}) # focus REPL
-        self.window.run_command('repl_enter')
-        self.window.run_command('focus_group', {"group": 0})
-        self.window.run_command('move', {
-            "by": "lines",
-            "forward": True,
-            "extend": False
-            })
+        v.window().run_command('focus_group', {"group": 1}) # focus REPL
+        v.window().run_command('repl_enter')
+        v.sel().clear()
 
-        # get the active view
-        v = self.window.active_view()
+        # collapse the cursor to the last selection point
+        v.window().run_command('focus_group', {"group": 0}) # focus REPL
 
-        # we then take the line where the cursor is. if empty, keep hitting enter.
-        while v.line(v.sel()[0]).empty():
-            self.window.run_command('focus_group', {"group": 1}) # focus REPL
-            self.window.run_command('repl_enter')
-            self.window.run_command('focus_group', {"group": 0})
-            self.window.run_command('move', {
-                "by": "lines",
-                "forward": True,
-                "extend": False
-                })
+        # move down to next non empty line
+        if not max_point_region.empty():
+            v.sel().clear()
+            max_point_line_num = max_point_line_num + 1
+            max_point_region = v.line(v.text_point(max_point_line_num, 0)) # region of max point
+
+            # if next line IS empty, then clear through white space
+            if max_point_region.empty():
+                #clear the console
+                v.window().run_command('focus_group', {"group": 1}) # focus REPL
+                v.window().run_command('repl_enter')
+
+                while max_point_region.empty():
+                    v.sel().clear()
+                    max_point_line_num = max_point_line_num + 1
+                    max_point_region = v.line(v.text_point(max_point_line_num, 0))
+                    v.sel().add(max_point_region.begin())
+                    if max_point_line_num >= eof_line:
+                        break
+
+                v.window().run_command('focus_group', {"group": 0}) # focus REPL
+            else:
+                v.sel().add(max_point_region.begin())
+        else:
+            while max_point_region.empty():
+                v.sel().clear()
+                max_point_line_num = max_point_line_num + 1
+                max_point_region = v.line(v.text_point(max_point_line_num, 0))
+                v.sel().add(max_point_region.begin())
+                if max_point_line_num >= eof_line:
+                    break
+
+    def keep_code_blocks(self):
+        selection = self.view.line(self.view.sel()[0]) # get the region, ordered
+
+        # find points of three quotes contained in the selection
+        three_quotes_pattern = self.view.find_all('(\"\"\")')
+        three_quotes = [q for q in three_quotes_pattern if selection.contains(q)]
+
+        # find comment-only lines
+        comment_pattern = self.view.find_all('^#.*')
+        comment_lines = [q for q in comment_pattern if selection.contains(q)]
+
+        # create blocks by finding three quote pairs
+        def find_comment_blocks(list):
+            start_points = []
+            end_points = []
+
+            for i, region in enumerate(three_quotes):
+                if (i+1)%2 == 1:
+                    start_points.append(region.a)
+                else:
+                    end_points.append(region.b)
+
+            comment_blocks = []
+            for val in zip(start_points, end_points):
+                block = self.view.lines(sublime.Region(val[0], val[1]))
+                comment_blocks.extend(block)
+
+            return comment_blocks
+
+        all_selected_lines = self.view.lines(self.view.sel()[0])
+        comment_blocks = find_comment_blocks(three_quotes)
+
+        # create comment indeces
+        comment_block_index = [x in comment_blocks for x in all_selected_lines]
+        comment_line_index = [x in comment_lines for x in all_selected_lines]
+
+        # removed comments
+        comments_removed = [a for a,b,c
+            in zip(all_selected_lines, comment_block_index,
+            comment_line_index) if b == c]
+
+        print('comment blocks:\n%s' % comment_block_index)
+        print('comment lines:\n%s' % comment_line_index)
+        print('keep the following lines:\n%s' % comments_removed)
+
+        # find empty lines and whitespace blocks
+        empty_index = [x.empty() for x in comments_removed] # find empty lines
+        whitespace_index = []
+        for i, x in enumerate(empty_index):
+            if x != empty_index[i-1]:
+                whitespace_index.append(False)
+            else:
+                whitespace_index.append(x)
+
+        print('empty lines:\n%s' % empty_index)
+        print('whitespace lines:\n%s' % whitespace_index)
+
+        # remove whitespace
+        keep_lines = [a for a,b
+            in zip(comments_removed, whitespace_index) if not b]
+
+
+        self.view.sel().clear() # clear current unordered selection
+        self.view.sel().add_all(keep_lines)
+
+        return self.view.sel()
+
+
+
+
+
+
+
+
+
+
+
 
 
